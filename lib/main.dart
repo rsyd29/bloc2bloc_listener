a9541d4ed904dc655cbf70253eca1478251f24c4
rsyd29 <brasyid15@gmail.com>
2022-08-07 17:11:51 +0700

    Implement bloc with listener

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,7 @@ class MyApp extends StatelessWidget {
           create: (context) => ColorBloc(),
         ),
         BlocProvider<CounterBloc>(
-          create: (context) => CounterBloc(
-            colorBloc: context.read<ColorBloc>(),
-          ),
+          create: (context) => CounterBloc(),
         ),
       ],
       child: MaterialApp(
@@ -35,47 +33,72 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int incrementSize = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.watch<ColorBloc>().state.color,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('Change Color'),
-              onPressed: () {
-                context.read<ColorBloc>().add(ChangeColorEvent());
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              '${context.watch<CounterBloc>().state.counter}',
-              style: TextStyle(
-                fontSize: 52.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+    return BlocListener<ColorBloc, ColorState>(
+      listener: (context, colorState) {
+        if (colorState.color == Colors.red) {
+          incrementSize = 1;
+        } else if (colorState.color == Colors.green) {
+          incrementSize = 10;
+        } else if (colorState.color == Colors.blue) {
+          incrementSize = 100;
+        } else if (colorState.color == Colors.black) {
+          incrementSize = -100;
+          context
+              .read<CounterBloc>()
+              .add(ChangeCounterEvent(incrementSize: incrementSize));
+        }
+      },
+      child: Scaffold(
+        backgroundColor: context.watch<ColorBloc>().state.color,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text('Change Color'),
+                onPressed: () {
+                  context.read<ColorBloc>().add(ChangeColorEvent());
+                },
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              child: Text(
-                'Increment Center',
-                style: TextStyle(fontSize: 24.0),
+              SizedBox(
+                height: 20.0,
               ),
-              onPressed: () {
-                context.read<CounterBloc>().add(ChangeCounterEvent());
-              },
-            ),
-          ],
+              Text(
+                '${context.watch<CounterBloc>().state.counter}',
+                style: TextStyle(
+                  fontSize: 52.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                child: Text(
+                  'Increment Center',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                onPressed: () {
+                  context
+                      .read<CounterBloc>()
+                      .add(ChangeCounterEvent(incrementSize: incrementSize));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
